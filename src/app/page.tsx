5d6f11221b8be1fdae0b402dc6cab056f199d927
tonychill <tony@ilanlyfe.com>
2024-02-04 21:10:08 -0500

    added more goodies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Hero from "@/components/hero";
+import Updates from "@/components/updates";
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Hero />
+      <Updates />
+    </div>
+  );
 }
